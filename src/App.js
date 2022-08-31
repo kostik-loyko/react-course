d2aecdc4b84859bfbad295a16184e38551546f85
kostik-loyko <kostik.loyko@mail.ru>
2022-08-31 22:36:10 +0300

    feat: add btn for creating new post

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import PostList from './components/PostList';
 import MyButton from './components/UI/buttom/MyButton';
 import MyInput from './components/UI/input/MyInput';
@@ -10,12 +10,29 @@ function App() {
     { id: 2, title: 'Javascript 2', body: 'Description 2' },
     { id: 3, title: 'Javascript 3', body: 'Description 3' },
   ]);
+
+  const [post, setPost] = useState({ title: '', body: '' });
+
+  const addNewPost = (e) => {
+    e.preventDefault();
+    setPosts([...posts, { ...post, id: Date.now() }]);
+    setPost({ title: '', body: '' });
+  }
+
   return (
     <div className='App'>
       <form>
-        <MyInput type='text' placeholder='Post name' />
-        <MyInput type='text' placeholder='Post description' />
-        <MyButton disabled>Create post</MyButton>
+        <MyInput
+          value={post.title}
+          onChange={e => setPost({ ...post, title: e.target.value })}
+          type='text'
+          placeholder='Post name' />
+        <MyInput
+          value={post.body}
+          onChange={e => setPost({ ...post, body: e.target.value })}
+          type='text'
+          placeholder='Post description' />
+        <MyButton onClick={addNewPost} >Create post</MyButton>
       </form>
       <PostList posts={posts} title={'List post'} />
     </div>
--- src/components/UI/input/MyInput.jsx
+++ src/components/UI/input/MyInput.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import classes from './MyInput.module.css'
+import classes from './MyInput.module.css';
 
-const MyInput = (props) => {
+const MyInput = React.forwardRef((props, ref) => {
   return (
-    <input className={classes.MyInput} {...props} />
+    <input ref={ref}{...props} className={classes.MyInput} />
   );
-}
+});
+
 export default MyInput;
